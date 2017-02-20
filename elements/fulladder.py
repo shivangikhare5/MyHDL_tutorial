a830bc0fe9f045ea818d4a87dd369abf513e2a52
GitLab <ellyptech.com@gmail.com>
2017-02-20 09:43:59 +0530

    update

--- elements/fulladder.py
+++ elements/fulladder.py
@@ -3,11 +3,32 @@ from myhdl import *
 @block
 def bit_adder(a, b, cin, s, cout):
     
-    @always(a, b, cin)
+    @always_comb
     def logic():
-        print ('#',a, b, cin)
-        pass
-        #s.next    = a ^ b ^ cin
-        #cout.next = (a & b) | ((a ^ b) & cin)
+        s.next    = a ^ b ^ cin
+        cout.next = (a & b) | ((a ^ b) & cin)
+        
+    return instances()
+
+@block
+def bit_adder_vec(a, b, cin, s, cout, DATA=2):
+    c = [Signal(bool(0)) for i in range(DATA+1)]
+    sv = [Signal(bool(0)) for i in range(DATA)]
+    addr_inst = [None for i in range(DATA)]
+    
+    for i in range(DATA):
+        addr_inst[i] = bit_adder(a(i), b(i), c[i], sv[i], c[i+1])
+        
+    p = [sv[i] for i in reversed(range(DATA))]
+    q = ConcatSignal(*p) 
+    
+    @always_comb
+    def logic0():
+        c[0].next = cin
+        s.next = q
+        
+    @always_comb
+    def logic1():
+        cout.next = c[DATA]
         
     return instances()
--- elements/unittests/test_fulladder.py
+++ elements/unittests/test_fulladder.py
@@ -14,7 +14,7 @@ class TestFullAdder(TestCase):
         def testbench():
             a, b, s, cin, cout = [Signal(bool(0)) for i in range(5)]
             clk = Signal(bool(0))
-            addr_inst = fulladder.bit_adder(a, b, s, cin, cout)
+            addr_inst = fulladder.bit_adder(a, b, cin, s, cout)
             
             @always(delay(10))
             def clkgen():
@@ -23,20 +23,53 @@ class TestFullAdder(TestCase):
             @instance
             def write():
                 a.next    = bool(0)
-                b.next    = 0
-                cin.next  = 0
+                b.next    = bool(0)
+                cin.next  = bool(0)
                 yield delay(50)      
                 for i in range(10):
                     yield clk.posedge
                     a.next = bool(randrange(2))
                     b.next = bool(randrange(2))
                     cin.next = bool(randrange(2))
-                    #self.assertEqual(s, a ^ b ^ cin)
-                    #self.assertEqual(cout, (a & b) | ((a ^ b) & cin))
-                    print(int(a), int(b), int(cin), int(s))
+                    self.assertEqual(s, a ^ b ^ cin)
+                    self.assertEqual(cout, (a & b) | ((a ^ b) & cin))
+                    #print(int(a), int(b), int(cin), int(s), int(cout))
             return instances()        
         
         tb = testbench()
+        tb.config_sim(trace=True)
+        tb.run_sim(duration=1500)
+        tb.quit_sim()
+    def testFullAdderVec(self):
+        DATA = 8
+        @block
+        def testbenchAdderVec():            
+            a, b, s = [Signal(modbv(0)[DATA:]) for i in range(3)]
+            cin, cout = [Signal(bool(0)) for i in range(2)]
+            clk = Signal(bool(0))
+            addr_inst = fulladder.bit_adder_vec(a, b, cin, s, cout)
+            
+            @always(delay(10))
+            def clkgen():
+                clk.next = not clk
+            
+            @instance
+            def write():
+                a.next    = 0
+                b.next    = 0
+                cin.next  = 0
+                yield delay(50)      
+                for i in range(10):
+                    yield clk.posedge
+                    a.next = randrange(2**DATA)
+                    b.next = randrange(2**DATA)
+                    cin.next = randrange(2)
+                    #self.assertEqual(s+ (cout << DATA), a ^ b ^ cin)
+                    #self.assertEqual(cout, (a & b) | ((a ^ b) & cin))
+                    print(int(a), int(b), int(cin), int(s), int(cout), int(s+ (cout << (DATA-1))))
+            return instances()        
+        
+        tb = testbenchAdderVec()
         tb.config_sim(trace=True)
         tb.run_sim(duration=1500)
         tb.quit_sim()
