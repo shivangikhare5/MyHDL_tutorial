581e3277ce738e315dfaa4de2c7aa3820beea855
Shilpimish <shilpimishra@iiitdmj.ac.in>
2017-03-10 10:38:59 +0530

    Add files via upload

--- elements/lfsr.py
+++ elements/lfsr.py
@@ -1,7 +1,9 @@
-from myhdl import block,always_comb,always
+from myhdl import block,always_comb,always,instance,Signal,intbv,delay
 
 @block
-def fsr(bit,lfsr,clk):
+def fsr(bit,lfsr,load,clk,rst):
+
+	seed = Signal(intbv(0xACE1)[16:])
 
 	@always_comb
 	def tap():
@@ -9,10 +11,14 @@ def fsr(bit,lfsr,clk):
 		'''bit.next = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1'''
         
 	
-	@always(clk.posedge)
+	@always(clk.posedge,rst.posedge)
 	def logic():
-		lfsr.next =  (lfsr >> 1) | (bit << 15)
-				
+		if load:
+			lfsr.next = seed
+		else:
+			lfsr.next =  (lfsr >> 1) | (bit << 15)
+		
+	
 	return tap,logic
 		
 
